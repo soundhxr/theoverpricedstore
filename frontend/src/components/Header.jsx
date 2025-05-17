02c44170f5fb838e7f0eea75eb4700778e6b16be
soundhxr <50undh3r@gmail.com>
2025-05-17 19:58:55 +0530

    add login and register UI for auth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,37 +1,70 @@
-import { Nav, Navbar, Container, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 import logo from '../assets/logo.png';
 
 const Header = () => {
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      // NOTE: here we need to reset cart state for when a user logs out so the next
+      // user doesn't inherit the previous users cart and shipping
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to='/'>
-            <img src={logo} alt='logo' style={{ height: '30px', marginRight: '10px' }} />
-            TheOverpricedStore
+            <img src={logo} alt='TheOverpricedStore' />
+            The Overpriced Store
           </Navbar.Brand>
-
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               <Nav.Link as={Link} to='/cart'>
                 <FaShoppingCart /> Cart
-                {
-                  cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
-                      { cartItems.reduce((a,c) => a + c.qty, 0)}
-                    </Badge>
-                  )
-                }
+                {cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
               </Nav.Link>
-              <Nav.Link as={Link} to='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <NavDropdown.Item as={Link} to='/profile'>
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              )}
+
+              
+              
             </Nav>
           </Navbar.Collapse>
         </Container>

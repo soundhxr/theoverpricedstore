c80d5c490826a73bb2ffefe1f73ca55136dfaaaa
soundhxr <50undh3r@gmail.com>
2025-05-17 18:16:16 +0530

    setup backend auth logic with JWT

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT;
 
@@ -10,11 +12,19 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })); 
+
+// cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(errorHandler);
 app.use(notFound);
